226a8abd747caf219fa947dda7f98ac537abd633
DBAA <hellocaoziyi@gmail.com>
2020-03-07 23:53:34 +0800

    First commit

---
+++ include/config.h
@@ -0,0 +1,9 @@
+/* Automatically generated - do not edit */
+#define CONFIG_IMX_CONFIG	board/freescale/mx6ull_caoziyi_nand/imximage.cfg
+#define CONFIG_SYS_BOOT_NAND	1
+#define CONFIG_BOARDDIR board/freescale/mx6ull_caoziyi_nand
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/mx6ull_caoziyi_nand.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
---
+++ include/generated/timestamp_autogenerated.h
@@ -0,0 +1,4 @@
+#define U_BOOT_DATE "Mar 07 2020"
+#define U_BOOT_TIME "20:49:12"
+#define U_BOOT_TZ "+0800"
+#define U_BOOT_DMI_DATE "03/07/2020"
